de361903b3b42ff49fa25139cf1468ab35ad8bc9
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-02-06 02:38:47 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -11,14 +11,24 @@ template<typename TValue>
 class Stack
 {
 private:
-	const size_t _maxSize{ DEFAULT_STACK_SIZE };
-	TValue _stack[DEFAULT_STACK_SIZE]{};
+	size_t _maxSize;
+	TValue* _stack;
 	size_t _counter{ 0 };
-public:
-	Stack() = default;
-	~Stack() = default;
 
-	void push(TValue element) { if (_counter == _maxSize) { return; } _stack[_counter++] = element; }
+	void _resize()
+	{
+		_maxSize += DEFAULT_STACK_SIZE;
+
+		TValue* newStack = new TValue[_maxSize];
+		for (size_t i = 0; i < _counter; i++) { newStack[i] = _stack[i]; }
+		delete[] _stack; _stack = newStack;
+	}
+public:
+	Stack() : _maxSize(DEFAULT_STACK_SIZE) { _stack = new TValue[_maxSize]; }
+	Stack(const size_t& maxSize) : _maxSize(maxSize), Stack() {}
+	~Stack() { delete[] _stack; }
+
+	void push(TValue element) { if (_counter == _maxSize) { _resize(); } _stack[_counter++] = element; }
 	TValue pop() { if (_counter > 0) { return _stack[--_counter]; } return TValue(); }
 	TValue top() { return (_counter != 0) ? _stack[_counter - 1] : TValue(); }
 	TValue clear() { _counter = 0; return TValue(); }
@@ -36,7 +46,7 @@ int main()
 	cout << "  Push 'a'. Result: "; stack.push('a'); cout << stack.top() << endl;
 	cout << "  Push 'b'. Result: "; stack.push('b'); cout << stack.top() << endl;
 	cout << "  Push 'c'. Result: "; stack.push('c'); cout << stack.top() << endl;
-	cout << "  Push 'd'. Result: "; stack.push('c'); cout << stack.top() << endl;
+	cout << "  Push 'd'. Result: "; stack.push('d'); cout << stack.top() << endl;
 	cout << endl;
 	
 	cout << "Checks: " << endl;
@@ -63,10 +73,18 @@ int main()
 	cout << "  Push 'b'. Result: "; stack.push('b'); cout << stack.top() << endl;
 	cout << "  Counting the number of characters in the stack. Result: " << stack.getCounter() << endl;
 	cout << "  Push 'c'. Result: "; stack.push('c'); cout << stack.top() << endl;
-	cout << "  Push 'd'. Result: "; stack.push('c'); cout << stack.top() << endl;
+	cout << "  Push 'd'. Result: "; stack.push('d'); cout << stack.top() << endl;
+	cout << "  Push 'e'. Result: "; stack.push('e'); cout << stack.top() << endl;
+	cout << "  Push 'f'. Result: "; stack.push('f'); cout << stack.top() << endl;
+	cout << "  Push 'g'. Result: "; stack.push('g'); cout << stack.top() << endl;
+	cout << "  Push 'h'. Result: "; stack.push('h'); cout << stack.top() << endl;
+	cout << "  Push 'j'. Result: "; stack.push('j'); cout << stack.top() << endl;
 	cout << endl;
 
+	cout << "Max size. Result: " << stack.getSize() << endl;
+	cout << "Counting the number of characters in the stack. Result: " << stack.getCounter() << endl;
 	cout << "Clear the stack. Result (isEmpty): "; stack.clear(); (stack.isEmpty()) ? cout << "true" : cout << "false"; cout << endl;
+	cout << "Max size. Result: " << stack.getSize() << endl;
 	cout << "Counting the number of characters in the stack. Result: " << stack.getCounter() << endl;
 	cout << endl;
 
